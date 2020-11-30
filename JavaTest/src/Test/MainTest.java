e409a9d077f680e507c39fa171482294ebbe8397
xin-2020 <1713695034@qq.com>
2020-11-30 17:04:31 +0800

    在eclipse中发布

---
+++ JavaTest/src/Test/MainTest.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello,");
+		System.out.println("java是这个世界上最牛逼的语言！");
+		
+	}
+
+}
